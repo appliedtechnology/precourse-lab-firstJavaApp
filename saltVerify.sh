c0633b27d283b6bb3f5dc2f2f13b35467cf460f8
Marcus Hammarberg <marcusoft.net@gmail.com>
2022-06-21 15:47:21 +0200

    Initial commit

--- saltVerify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-# cd to the location of the script
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-cd $SCRIPTPATH
-
-echo "Nothing to test yet"
-exit 0
---
+++ src/main/java/se/salt/precourse/firstJavaApp/Greeter.java
@@ -0,0 +1,22 @@
+package se.salt.precourse.firstJavaApp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Greeter {
+  private static String greet(String namePassedIn) {
+    return "Welcome to SALT, ";
+  }
+
+  public static void main(String[] args) throws IOException {
+    System.out.print("What is your name? ");
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String name = reader.readLine();
+
+    String greeting = greet(name);
+
+    System.out.println(greeting);
+  }
+}
